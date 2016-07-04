a3cbeef2f01cbafb4dd4bdb7ccb63819234579a9
Kappisan <wilkosz.ian@gmail.com>
2016-07-04 01:02:01 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+var app = angular.module('app', ['ngRoute'])
+	.config( ['$routeProvider', function($routeProvider) {
+		$routeProvider
+			.when('/', { templateUrl: 'templates/home.html' })	
+			.when('/about', { templateUrl: 'templates/about.html' })	
+			.when('/justynas_christmas_chaos', { templateUrl: 'templates/justynas_christmas_chaos.html' })
+			.when('/twistfour', { templateUrl: 'templates/twistfour.html' })
+			.otherwise({ redirectTo: '/' });
+	}]);
+
+
+// CONTROLLERS
+
+
+app.controller('PageCtrl', ['$scope', '$http', '$location', function($s, $http, $location) {
+
+	console.log("location", $location.$$path);
+	$s.currentPage = $location.$$path;
+
+
+
+	$s.changeCurrentPage = function(page) {
+		console.log("change", page);
+
+		$('.nav-menu-item').removeClass('active');
+
+		switch(page) {
+			case('about'): 
+				window.location = "#/about"; 
+				$('#nav-menu-about').addClass('active');
+				break;
+			case('work'): 
+				window.location = "#/work_experience"; 
+				$('#nav-menu-work').addClass('active');
+				break;
+			case('skills'): 
+				window.location = "#/skills"; 
+				$('#nav-menu-skills').addClass('active');
+				break;
+		}
+	}
+
+$( document ).ready(function() {
+
+	if($location.$$path== "/about") {
+		$('#nav-menu-about').addClass('active');
+	}
+	if($location.$$path== "/work_experience") {
+		$('#nav-menu-work').addClass('active');
+	}
+	if($location.$$path== "/skills") {
+		$('#nav-menu-skills').addClass('active');
+		console.log("matched");
+	}
+});
+
+
+}]);
+
+
